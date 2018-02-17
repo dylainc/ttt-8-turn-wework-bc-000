8bcd9163ccb8f8ab7820868adb385f06079c3002
Dylain Cohen <dylain.cohen@wework.com>
2018-02-17 20:42:32 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -26,8 +26,8 @@ def position_taken?(board, index)
   end
 end
 
-def move(board, index, token = "X")
-  board[index] = token
+def move(board, input, token = "X")
+  board[input_to_index(input)] = token
 end
 
 def input_to_index(input)
