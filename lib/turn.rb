628ac98a1349c72e20fcbd38e4d1bbfde6308c89
Dylain Cohen <dylain.cohen@wework.com>
2018-02-17 21:26:05 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,10 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
 
+  if valid_move?(board,index) == true
+    move(board, index, token)
+  else
+    turn(board)
+  end
 
-
-  #move(board, index, token)
 end
