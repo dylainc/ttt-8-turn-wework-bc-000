9e47508abac1bbe761934b94593fb11a893afc62
Dylain Cohen <dylain.cohen@wework.com>
2018-02-17 21:17:05 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
 
-  if index(0,8) == true
+  if index.between?(0,8) == true
     move(board, index, token)
     display_board(board)
   else
